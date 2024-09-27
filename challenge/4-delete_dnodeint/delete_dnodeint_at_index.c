a4eca8ff76c270f4fd6ebd46db55c581c91fe31e
AnthonyCointre <8732@holbertonstudents.com>
2024-09-27 11:01:54 +0200

    move files

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -16,40 +16,37 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int p;
 
 	if (*head == NULL)
+	{
 		return (-1);
-
+	}
 	saved_head = *head;
 	p = 0;
-
 	while (p < index && *head != NULL)
 	{
 		*head = (*head)->next;
 		p++;
 	}
-
 	if (p != index)
 	{
 		*head = saved_head;
 		return (-1);
 	}
-
 	if (0 == index)
 	{
 		tmp = (*head)->next;
 		free(*head);
 		*head = tmp;
 		if (tmp != NULL)
+		{
 			tmp->prev = NULL;
+		}
 	}
 	else
 	{
-		if ((*head)->prev)
-			(*head)->prev->next = (*head)->next;
-
+		(*head)->prev->prev = (*head)->prev;
+		free(*head);
 		if ((*head)->next)
 			(*head)->next->prev = (*head)->prev;
-
-		free(*head);
 		*head = saved_head;
 	}
 	return (1);
